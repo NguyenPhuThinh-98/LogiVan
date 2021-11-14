bd4f7b95943817b4d9cee3e27fc9c3ea0fc65efa
NguyenPhuThinh-98 <89929016+NguyenPhuThinh-98@users.noreply.github.com>
2021-11-14 20:56:23 +0700

    Create Loginow.aspx

---
+++ LogiVan/loginow.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LogiVan
+{
+    public partial class loginow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("loginow.aspx#register");
+        }
+    }
+}
